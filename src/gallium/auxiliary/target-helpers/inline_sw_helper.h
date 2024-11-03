73a9ffb466394bd8e3913de4338d7d5af1734f7a
Vera-Firefly <1964762970@qq.com>
2024-11-03 15:25:06 +0800

    测试

--- src/gallium/auxiliary/target-helpers/inline_sw_helper.h
+++ src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -80,11 +80,11 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 
 #if defined(GALLIUM_FREEDRENO)
    if (screen == NULL && strcmp(driver, "freedreno") == 0) {
-      int kbase_device_fd = open("/dev/kgsl-3d0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
-      if (kbase_device_fd == -1) {
-         printf("FD_OSMESA: Failed to open kbase device: %s", strerror(errno));
-         return NULL;
-      }
+      /** int kbase_device_fd = open("/dev/kgsl-3d0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+       * if (kbase_device_fd == -1) {
+       *    printf("FD_OSMESA: Failed to open kbase device: %s", strerror(errno));
+       *    return NULL;
+       */}
       struct pipe_screen_config dummy_cfg = { NULL, NULL };
       // screen = fd_screen_create(kbase_device_fd, &dummy_cfg, NULL);
       screen = fd_screen_create(3, &dummy_cfg, NULL);
@@ -93,13 +93,14 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 
 #if defined(GALLIUM_PANFROST)
    if (screen == NULL && strcmp(driver, "panfrost") == 0) {
-      int kbase_device_fd = open("/dev/mali0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
-      if (kbase_device_fd == -1) { 
-         printf("PAN_OSMESA: Failed to open kbase device: %s", strerror(errno));
-      } else {
-        struct pipe_screen_config dummy_cfg = { NULL, NULL };
-      	screen = panfrost_create_screen(kbase_device_fd, &dummy_cfg, NULL);
-      }
+      /** int kbase_device_fd = open("/dev/mali0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+       * if (kbase_device_fd == -1) { 
+       *    printf("PAN_OSMESA: Failed to open kbase device: %s", strerror(errno));
+       *    return NULL;
+       */}
+      struct pipe_screen_config dummy_cfg = { NULL, NULL };
+      // screen = panfrost_create_screen(kbase_device_fd, &dummy_cfg, NULL);
+      screen = panfrost_create_screen(3, &dummy_cfg, NULL);
    }
 #endif
 
